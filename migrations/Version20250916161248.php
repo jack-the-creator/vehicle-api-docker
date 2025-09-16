120b2ad55552e99e66203ce667721ba28e5f6151
Jack Arnold <jackarnold@Mac.mynet>
2025-09-16 17:42:23 +0100

    Initial commit - created User and JWT authentication

---
+++ migrations/Version20250916161248.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250916161248 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('INSERT INTO "user" (username, roles, password) VALUES (\'test.user\', \'["ROLE_USER"]\', \'$2y$13$eDUSGbunZAXMALCV86/MBeg19b87PRqYD97q9oBVYwbsdzkyzsogO\')');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+    }
+}
